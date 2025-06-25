41d030f040ccbd79aa57dac427cd772fc962b823
VictorCervs <victorcervantes.info@gmail.com>
2025-06-25 17:30:55 -0600

    feat: Crear una lista de compras

---
+++ lista_compras.py
@@ -0,0 +1,16 @@
+#1 Crear una lista de compras con 4 articulos
+lista_de_compras = ["huevos", "leche", "jugo", "papel"]
+
+#2 Imprimir la lista con un mensaje descriptivo
+print(f"Mi lista de compras inicial es: {lista_de_compras}")
+
+#3 Imprimir el primer y tercer elemento de la lista en una sola linea
+print(f"No debo olvidar comprar {lista_de_compras[0]} y  {lista_de_compras[2]}")
+
+#4 Añadir un nuevo articulo al final de la lista
+print("¡Ah! Se me olvidaba el café")
+lista_de_compras.append("café")
+
+#5 Imprimir la lista completa 
+
+print(f"Mi lista de compras actualizada es: {lista_de_compras}")
