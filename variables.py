a35629b06e76aea5d7d241053275ebc4175e8c49
VictorCervs <victorcervantes.info@gmail.com>
2025-06-25 13:25:13 -0600

    Agregado scrpti de variables y tipos de datos

---
+++ variables.py
@@ -0,0 +1,35 @@
+#1 Variables para almacenar datos personales
+mi_nombre = "Victor cervantes"
+mi_edad = 23 # Edad real
+mi_estatura_metros = 1.63 # Estatura real
+es_estudiante = True
+
+#2 Variables para información de un productor
+nombre_producto = "Laptop Gaming x"
+precio_producto= 1200.50
+stock_disponible = 10
+esta_en_oferta = False
+
+#3 Imprime el valor de cada variable
+print("---Información Personal---")
+print("Nombre", mi_nombre)
+print("Edad", mi_nombre)
+print("Estatura(metros)", mi_estatura_metros)
+print("Es estudiante", es_estudiante)
+
+print("\n--- Información del producto ---")
+print("Nombre del prodcuto", nombre_producto)
+print("Precio", precio_producto)
+print("Stock", stock_disponible)
+print("En oferta:", esta_en_oferta)
+
+#4 Imprime el tipo de cada variable para verificar
+print("\n--- Tipo de datos ---")
+print("Tipo de mi_nombre", type(mi_nombre))
+print("Tipo de mi_edad", type(mi_edad))
+print("Tipo de mi_estatura_metros", type(mi_estatura_metros))
+print("Tipo de es_estudiante", type(es_estudiante))
+print("Tipo de nombre_producto", type(nombre_producto))
+print("Tipo de precio_producto", type(precio_producto))
+print("Tipo de stock_disponible", type (stock_disponible))
+print("Tipo de esta_en_oferta", type (esta_en_oferta))
