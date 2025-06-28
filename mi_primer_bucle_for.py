32ed06b91f221d3fa89d3dfae6429e11c575939f
VictorCervs <victorcervantes.info@gmail.com>
2025-06-28 02:49:10 -0600

    Actualizacion bucle

--- mi_primer_bucle_for.py
+++ mi_primer_bucle_for.py
@@ -1,3 +1,19 @@
-mis_cosas_favoritas = ["libros","Comida","Música"]
-for cosas in mis_cosas_favoritas:
-    print(f"{cosas}")
+#INICIO
+  # ¿Qué creas primero? (La lista)
+
+mis_tareas = ["Tomar notas", "Comer", "Correr"]
+
+  # ¿Qué creas después? (El contador)
+
+contador = 1
+
+# Ahora, la estructura del bucle...
+
+for tareas in mis_tareas:
+    print(f"{contador}. {tareas}")
+    contador+= 1
+
+      # ¿Qué haces primero aquí adentro? (La acción de MOSTRAR)
+      # ¿Qué haces después? (La acción de INCREMENTAR)
+      
+#FIN
