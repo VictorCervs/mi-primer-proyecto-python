a95c502c4bbeb6132ea6438d259b2505515edb35
VictorCervs <victorcervantes.info@gmail.com>
2025-06-29 18:11:04 -0600

    Refuerzo de bucle for in2 exit()

---
+++ marcador.py
@@ -0,0 +1,10 @@
+puntosPorCuarto = [10,8,5]
+contador = 1
+
+print("Contadores en el cuarto")
+
+for cuarto in puntosPorCuarto:
+
+    print(f"Puntos en el cuarto{contador}: {cuarto}")
+    contador += 1
+
